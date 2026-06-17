e9924b579600b1a667814f9b8bf2c771727ebb39
Leonardo Barbosa <leonardotuio@hotmail.com>
2026-06-16 21:37:34 -0300

    Java

---
+++ Java/bombaBet/bombaBet/src/test/java/com/example/bombaBet/BombaBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bombaBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombaBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
