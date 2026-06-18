6507312c64511e4471bc50641529b51510244048
LeoBelluci <Leonardotuio@hotmail.com>
2026-06-18 18:38:07 -0300

    SelecaoRepository configurado

--- Java/bombaBet/src/main/java/com/example/bombaBet/repository/SelecaoRepository.java
+++ Java/bombaBet/src/main/java/com/example/bombaBet/repository/SelecaoRepository.java
@@ -3,6 +3,21 @@ package com.example.bombaBet.repository;
 import com.example.bombaBet.model.Selecao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SelecaoRepository
         extends JpaRepository<Selecao, Long> {
+
+    Optional<Selecao> findByCodigoFifaIgnoreCase(
+            String codigoFifa
+    );
+
+    boolean existsByCodigoFifaIgnoreCase(
+            String codigoFifa
+    );
+
+    List<Selecao> findByNomeContainingIgnoreCase(
+            String nome
+    );
 }
