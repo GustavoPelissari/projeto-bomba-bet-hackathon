5e1fe3fa615e7397bb7807d948031c321070ae91
Bruno Madeira <brunomadeira044@gmail.com>
2026-06-17 22:39:55 -0300

    Adicionando PalpiteRepository

---
+++ Java/bombaBet/src/main/java/com/example/bombaBet/repository/PalpiteRepository.java
@@ -0,0 +1,28 @@
+package com.example.bombaBet.repository;
+
+import com.example.bombaBet.model.Palpite;
+import com.example.bombaBet.model.Partida;
+import com.example.bombaBet.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PalpiteRepository
+        extends JpaRepository<Palpite, Long> {
+
+    List<Palpite> findByUsuario(
+            Usuario usuario
+    );
+
+    List<Palpite> findByPartida(
+            Partida partida
+    );
+
+    Optional<Palpite> findByUsuarioAndPartida(
+            Usuario usuario,
+            Partida partida
+    );
+
+}
